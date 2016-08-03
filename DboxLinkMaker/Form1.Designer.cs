1a406e96d81094e3841bd3b59ef24367d77040d9
John Zoidberg <9zoiberg7@gmail.com>
2016-08-03 17:09:55 +0300

    version 1.0

--- DboxLinkMaker/Form1.Designer.cs
+++ DboxLinkMaker/Form1.Designer.cs
@@ -28,55 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.LinkList = new System.Windows.Forms.ListBox();
+            this.StatusPanel = new System.Windows.Forms.GroupBox();
+            this.StatusTextBox = new System.Windows.Forms.TextBox();
+            this.LinkListPanel = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.MainPanel = new System.Windows.Forms.GroupBox();
+            this.OutTxt = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.OutputLinkBox = new System.Windows.Forms.TextBox();
+            this.InputLinkBox = new System.Windows.Forms.TextBox();
+            this.StatusPanel.SuspendLayout();
+            this.LinkListPanel.SuspendLayout();
+            this.MainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // LinkList
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(420, 20);
-            this.textBox1.TabIndex = 0;
+            this.LinkList.FormattingEnabled = true;
+            this.LinkList.Location = new System.Drawing.Point(12, 19);
+            this.LinkList.Name = "LinkList";
+            this.LinkList.Size = new System.Drawing.Size(420, 17);
+            this.LinkList.TabIndex = 2;
+            this.LinkList.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
-            // textBox2
+            // StatusPanel
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 38);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(420, 20);
-            this.textBox2.TabIndex = 0;
+            this.StatusPanel.Controls.Add(this.OutTxt);
+            this.StatusPanel.Controls.Add(this.StatusTextBox);
+            this.StatusPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.StatusPanel.Location = new System.Drawing.Point(0, 108);
+            this.StatusPanel.Name = "StatusPanel";
+            this.StatusPanel.Size = new System.Drawing.Size(488, 29);
+            this.StatusPanel.TabIndex = 3;
+            this.StatusPanel.TabStop = false;
+            this.StatusPanel.Text = "Status";
+            // 
+            // StatusTextBox
+            // 
+            this.StatusTextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.StatusTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.StatusTextBox.Cursor = System.Windows.Forms.Cursors.Default;
+            this.StatusTextBox.ForeColor = System.Drawing.Color.Lime;
+            this.StatusTextBox.Location = new System.Drawing.Point(12, 12);
+            this.StatusTextBox.Name = "StatusTextBox";
+            this.StatusTextBox.ReadOnly = true;
+            this.StatusTextBox.Size = new System.Drawing.Size(458, 13);
+            this.StatusTextBox.TabIndex = 0;
+            // 
+            // LinkListPanel
+            // 
+            this.LinkListPanel.Controls.Add(this.button2);
+            this.LinkListPanel.Controls.Add(this.LinkList);
+            this.LinkListPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.LinkListPanel.Location = new System.Drawing.Point(0, 65);
+            this.LinkListPanel.Name = "LinkListPanel";
+            this.LinkListPanel.Size = new System.Drawing.Size(488, 43);
+            this.LinkListPanel.TabIndex = 4;
+            this.LinkListPanel.TabStop = false;
+            this.LinkListPanel.Text = "Converted Links";
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.Location = new System.Drawing.Point(435, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 24);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Export";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.button1);
+            this.MainPanel.Controls.Add(this.OutputLinkBox);
+            this.MainPanel.Controls.Add(this.InputLinkBox);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(488, 63);
+            this.MainPanel.TabIndex = 5;
+            this.MainPanel.TabStop = false;
+            // 
+            // OutTxt
+            // 
+            this.OutTxt.Location = new System.Drawing.Point(478, 19);
+            this.OutTxt.Multiline = true;
+            this.OutTxt.Name = "OutTxt";
+            this.OutTxt.ReadOnly = true;
+            this.OutTxt.Size = new System.Drawing.Size(10, 10);
+            this.OutTxt.TabIndex = 9;
+            this.OutTxt.Visible = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(438, 12);
+            this.button1.Location = new System.Drawing.Point(435, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(32, 46);
-            this.button1.TabIndex = 1;
+            this.button1.Size = new System.Drawing.Size(50, 46);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Do It!";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // OutputLinkBox
+            // 
+            this.OutputLinkBox.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.OutputLinkBox.Location = new System.Drawing.Point(12, 38);
+            this.OutputLinkBox.Name = "OutputLinkBox";
+            this.OutputLinkBox.ReadOnly = true;
+            this.OutputLinkBox.Size = new System.Drawing.Size(420, 20);
+            this.OutputLinkBox.TabIndex = 6;
+            // 
+            // InputLinkBox
+            // 
+            this.InputLinkBox.Location = new System.Drawing.Point(12, 12);
+            this.InputLinkBox.Name = "InputLinkBox";
+            this.InputLinkBox.Size = new System.Drawing.Size(420, 20);
+            this.InputLinkBox.TabIndex = 7;
+            this.InputLinkBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.InputLinkBox_MouseClick);
+            this.InputLinkBox.Leave += new System.EventHandler(this.InputLinkBox_Leave);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 69);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(488, 137);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.LinkListPanel);
+            this.Controls.Add(this.StatusPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Link Maker";
+            this.Text = "DropBox Link Converter v1.0 by |ZoidberG|";
+            this.TopMost = true;
+            this.StatusPanel.ResumeLayout(false);
+            this.StatusPanel.PerformLayout();
+            this.LinkListPanel.ResumeLayout(false);
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox LinkList;
+        private System.Windows.Forms.GroupBox StatusPanel;
+        private System.Windows.Forms.TextBox StatusTextBox;
+        private System.Windows.Forms.GroupBox LinkListPanel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox OutTxt;
+        private System.Windows.Forms.GroupBox MainPanel;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox OutputLinkBox;
+        private System.Windows.Forms.TextBox InputLinkBox;
     }
 }
 
--- DboxLinkMaker/Form1.cs
+++ DboxLinkMaker/Form1.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using System.Net;
+using System.IO.Compression;
+using System.Diagnostics;
 
 namespace DboxLinkMaker
 {
@@ -15,14 +14,105 @@ namespace DboxLinkMaker
         public Form1()
         {
             InitializeComponent();
+            CreateTips();
+        }
+
+        private void CreateTips()
+        {
+            InputLinkBox.ForeColor = Color.Gray;
+            InputLinkBox.Text = "Insert link here!";
+
+            OutputLinkBox.ForeColor = Color.Gray;
+            OutputLinkBox.Text = "Converted link will be here!";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text != "")
+            if (InputLinkBox.Text != "")
             {
+                string firstlink = "";
+                firstlink = InputLinkBox.Text.Remove(0,26);
+                firstlink = firstlink.Remove(firstlink.Length - 5, 5);
+                string secondlink = "https://dl.dropboxusercontent.com/s/" + firstlink + "?dl=1";
+                OutputLinkBox.Text = secondlink;
+                OutputLinkBox.SelectAll();
+                OutputLinkBox.Copy();
+                StatusTextBox.Text = "New link has been copied to clipboard!";
 
+                Int32 FirstValue = LinkList.Items.Count;
+
+                if (LinkList.Items.Contains(OutputLinkBox.Text))
+                { }
+                else
+                {
+                    LinkList.Items.Add(OutputLinkBox.Text);
+
+                }
+
+                Int32 SecondValue = LinkList.Items.Count;
+
+                if (SecondValue > FirstValue)
+                {
+                    if (LinkList.Items.Count >= 2)
+                    {
+                        LinkListPanel.Height = LinkListPanel.Height + 13;
+                        LinkList.Height = LinkList.Height + 13;
+                        this.Height = this.Height + 13;
+                    }
+                }
+
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           StatusTextBox.Text = LinkList.SelectedItem.ToString();
+            StatusTextBox.SelectAll();
+            StatusTextBox.Copy();
+            StatusTextBox.Text = "Copied to clipboard: " + LinkList.SelectedItem.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ExportLinksToFile();
+            MessageBox.Show("All links are exported in ExportedLinks.txt , which is in same directory as this application.");
+            Process.Start(Application.StartupPath);
+        }
+
+        private void ExportLinksToFile()
+        {
+            OutTxt.Text = "====== " + DateTime.Now.ToString("dd/MM/yyyy") + " / " + string.Format("{0:HH:mm:ss tt}", DateTime.Now) + "======" + Environment.NewLine;
+
+            foreach (var item in LinkList.Items)
+            {
+                OutTxt.Text = OutTxt.Text + item.ToString() + Environment.NewLine;              
+            }
+
+            OutTxt.Text = OutTxt.Text + "===================================" + Environment.NewLine;
+
+            if (File.Exists("ExportedLinks.txt"))
+                OutTxt.Text = OutTxt.Text + Environment.NewLine + File.ReadAllText("ExportedLinks.txt");
+
+            File.WriteAllText("ExportedLinks.txt", OutTxt.Text);
+        }
+
+        private void InputLinkBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (InputLinkBox.Text == "Insert link here!")
+            {
+                InputLinkBox.ForeColor = Color.Black;
+                InputLinkBox.Text = "";
+            }
+        }
+
+        private void InputLinkBox_Leave(object sender, EventArgs e)
+        {
+            if (InputLinkBox.Text == "")
+            {
+                InputLinkBox.ForeColor = Color.Gray;
+                InputLinkBox.Text = "Insert link here!";
             }
         }
     }
 }
+ 
